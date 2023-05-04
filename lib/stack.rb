27c0c9b3529422f533a100bdb02e5ee0fefcc274
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-04 15:53:19 +0100

    commit tree a4b553959df1eadc22f061746227077ecb7b7030

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,44 @@
-# your code here
+require 'byebug'
+class Stack
+
+    attr_reader :stack
+
+    attr_reader :limit
+
+    def initialize limit = nil
+        @stack = []
+        @limit = limit
+    end
+
+    def push element
+        if limit && stack.length >= limit
+            raise "Stack Overflow"
+        end
+        stack.push(element)
+    end
+
+    def pop
+        stack.pop
+    end
+
+    def peek 
+        stack.last
+    end
+
+    def size
+        stack.length
+    end
+
+    def empty?
+        stack.empty?
+    end
+
+    def full?
+        stack.length === limit
+    end
+
+    def search target
+        index = stack.rindex(target)
+        index ? stack.length - (index + 1): -1
+    end
+end
